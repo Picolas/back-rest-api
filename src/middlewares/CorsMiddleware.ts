6b0eb6a86d981b5f71b05fed3f1cf23b6a19011b
Nicolas <npiplard@gmail.com>
2023-04-19 11:05:16 +0200

    fix(NO-JIRA) fixed cors

--- src/middlewares/CorsMiddleware.ts
+++ src/middlewares/CorsMiddleware.ts
@@ -4,12 +4,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const ALLOWED_ORIGINS = process.env.APP_CORS_ALLOWED_ORIGINS || [];
-const corsOptions: any = {
+const corsOptions: cors.CorsOptions = {
     origin: function (origin: any, callback: any) {
-        if (ALLOWED_ORIGINS.indexOf(origin) !== -1) {
+        if (process.env.APP_ENVIRONMENT === 'development' || ALLOWED_ORIGINS.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
-            // return error if origin is not allowed
             callback(new Error('Not allowed by CORS'))
         }
     }
